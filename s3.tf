5c9907412e5798c07e611c0c8799deb0fbc32547
Varasheswar-reddy <varasheswar428@gmail.com>
2026-02-18 12:44:15 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafashaik1122.monobucket"
+  bucket = "varshuu.devops.project"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -25,7 +25,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "us-east-1"
-bucket = "mustafashaik1122.monobucket"
+bucket = "varshuu.devops.project"
 key = "prod/terraform.tfstate"
 }
 }
